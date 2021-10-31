9046b609097f7016e6d99edfc802034ff4225e09
sayatumarov <123466666z123466666z@gmail.com>
2021-10-31 10:13:28 +0600

    com

--- main.go
+++ main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"encoding/json"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"github.com/sayatumarov/sample_rest_api/serv"
 )
 
-func main() {
-	home := func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello")
+var db *sql.DB
+var err error
+
+type User struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func getPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var users []User
+	result, err := db.Query("SELECT id, name FROM users")
+	if err != nil {
+		panic(err.Error())
 	}
-	http.HandleFunc("/bar", home)
-	http.ListenAndServe(":8080", nil)
+	defer result.Close()
+	for result.Next() {
+		var user User
+		err := result.Scan(&user.ID, &user.Name)
+		if err != nil {
+			panic(err.Error())
+		}
+		users = append(users, user)
+	}
+	json.NewEncoder(w).Encode(users)
+	db.SetConnMaxLifetime(3)
+}
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/users", serv.GetUsers).Methods("GET")
+	http.ListenAndServe(":8000", r)
 }
--- server/serv.go
+++ server/serv.go
@@ -1,7 +1,43 @@
-package server
+package serv
 
-import "fmt"
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
 
-func Server() {
-	fmt.Println("Servak")
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type User struct {
+	Id       int
+	Name     string
+	Password string
+}
+
+var db *sql.DB
+var err error
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:8888)/simpleDB")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	w.Header().Set("Content-Type", "application/json")
+	var users []User
+	result, err := db.Query("SELECT id, name, password from users")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer result.Close()
+
+	for result.Next() {
+		var user User
+		er := result.Scan(&user.Id, &user.Name, &user.Password)
+		if er != nil {
+			panic(er.Error())
+		}
+		users = append(users, user)
+	}
+	json.NewEncoder(w).Encode(users)
 }
