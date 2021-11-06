e5c226a94cde873afaef0b8043f5c674158b2d41
sayatumarov <123466666z123466666z@gmail.com>
2021-11-06 13:11:26 +0600

    ...

--- getRequest/getUsers.go
+++ getRequest/getUsers.go
@@ -1,4 +1,4 @@
-package serv
+package getRequest
 
 import (
 	"encoding/json"
--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/sayatumarov/sample_rest_api/getRequest"
 )
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/users", getUsers.GetUsers).Methods("GET")
+	router.HandleFunc("/users", getRequest.GetUsers).Methods("GET")
 	http.ListenAndServe(":8000", router)
 }
